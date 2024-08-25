4ead4ed7b2f5a09cb2abcedb7ddad65f28aa3076
FreyRin <guilhermebertoldo58@gmail.com>
2024-08-24 21:19:05 -0300

    Finish Crud User

--- src/main/java/com/crudjava/crud/services/UserService.java
+++ src/main/java/com/crudjava/crud/services/UserService.java
@@ -26,4 +26,29 @@ public class UserService {
 		return user.orElseThrow(() -> new NoSuchElementException("usuário não encontrado: " + id));
 		
 	}
+	
+	public User create(User user) {
+		return userRepository.save(user);
+	}
+
+
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+		
+	}
+
+
+	public User update(Long id, User user) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity,user);
+		return userRepository.save(entity);
+		
+	}
+	
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPassword(user.getPassword());
+		entity.setDescription(user.getDescription());
+	}
 }
